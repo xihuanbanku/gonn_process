ce1d3663d84e63fa980c34059effe1f73c50ece8
xihuanbanku <liang870815>
2018-01-29 18:12:11 +0800

    init

---
+++ src/main/java/com/gonn/core/common/base/IBaseMapper.java
@@ -0,0 +1,8 @@
+package com.gonn.core.common.base;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+public interface IBaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
+
+}
